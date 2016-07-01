319c0c6b6f2c593812be11048d34c362321ad380
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-07-01 18:31:05 +0200

    package form: restore form data when validation fails

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -11,7 +11,8 @@ from ckanext.switzerland.helpers import (
     get_localized_org, localize_json_title, get_langs,
     get_frequency_name, get_terms_of_use_icon, get_dataset_terms_of_use,
     get_dataset_by_identifier, get_readable_file_size,
-    simplify_terms_of_use, parse_json, get_piwik_config
+    simplify_terms_of_use, parse_json, get_piwik_config,
+    convert_post_data_to_dict
 )
 
 import ckan.plugins as plugins
@@ -127,6 +128,7 @@ class OgdchPlugin(plugins.SingletonPlugin):
             'get_readable_file_size': get_readable_file_size,
             'get_piwik_config': get_piwik_config,
             'parse_json': parse_json,
+            'convert_post_data_to_dict': convert_post_data_to_dict,
         }
 
 
