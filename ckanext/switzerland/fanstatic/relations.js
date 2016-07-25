cd060b1e4fb88e9fa1d1fb36a58529b0990f4146
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-07-25 15:57:52 +0200

    forms: add remove buttons on relations form

--- ckanext/switzerland/fanstatic/relations.js
+++ ckanext/switzerland/fanstatic/relations.js
@@ -4,10 +4,14 @@ ckan.module('switzerland_relations', function ($, _) {
   return {
     initialize: function () {
       var relations_el = this.el;
+      var module = this;
 
+      module._init_remove_buttons();
+
+      // insert new form when add button is clicked
       relations_el.find('.add-button').click(function() {
         var last_form = relations_el.find('.relations-form:last');
-        var new_form = last_form.clone();
+        var new_form = last_form.clone(true);
 
         // replace id, name and for attributes on cloned form
         var last_index = parseInt(last_form.data('id'));
@@ -21,9 +25,28 @@ ckan.module('switzerland_relations', function ($, _) {
           });
         });
         new_form.data('id', last_index + 1);
+        new_form.find('input').val('');
         new_form.insertAfter(last_form);
+
+        // potentially show remove buttons on all forms when there was only one form before
+        module._init_remove_buttons();
         return false;
       });
+
+      // remove form on remove button press, potentially hiding the remove button when only one form is left
+      relations_el.find('.remove-button').click(function() {
+        $(this).parents('.relations-form').remove();
+        module._init_remove_buttons();
+      });
+    },
+
+    _init_remove_buttons: function() {
+      var remove_buttons = this.el.find('.remove-button');
+      if (remove_buttons.length < 2) {
+        remove_buttons.hide();
+      } else {
+        remove_buttons.show();
+      }
     }
   };
 });
