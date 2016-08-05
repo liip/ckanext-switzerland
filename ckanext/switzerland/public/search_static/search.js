0cb138e5b1c594ec92ade671ac93d39eec4001ea
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-08-05 15:34:17 +0200

    search: added ckan dataset search

--- ckanext/switzerland/public/search_static/search.js
+++ ckanext/switzerland/public/search_static/search.js
@@ -1,8 +1,32 @@
-console.log('hello world!');
 
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    currentSearchTerm: '',
+    searchTerm: '',
+    pageResults: [],
+    datasetResults: [],
+    language: ''
+  },
+  ready: function() {
+    var lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+    this.language = lang ? lang.split('-')[0] : 'en'
+  },
+  methods: {
+    search: function() {
+      var self = this;
+      self.currentSearchTerm = self.searchTerm;
+      $.ajax('/api/3/action/package_search?q=' + this.searchTerm).done(function(data) {
+        self.datasetResults = []
+        data.result.results.map(function(result) {
+          console.log(result)
+          self.datasetResults.push({
+            title: result.title[self.language],
+            description: result.description[self.language],
+            link: '/' + self.language + '/dataset/' + result.name
+          })
+        })
+      })
+    }
   }
 })
