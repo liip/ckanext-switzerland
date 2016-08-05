cfb0b5a5b13096b7a7630031ed5819e011600464
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-08-05 16:08:15 +0200

    search: make results from ckan and wordpress show up at the same time

--- ckanext/switzerland/public/search_static/search.js
+++ ckanext/switzerland/public/search_static/search.js
@@ -17,30 +17,31 @@ new Vue({
       var self = this;
       self.currentSearchTerm = self.searchTerm;
 
-      // ckan search
-      $.ajax('/api/3/action/package_search?q=' + this.searchTerm).done(function(data) {
+      var ckanSearch = $.ajax('/api/3/action/package_search?q=' + this.searchTerm);
+      var wordPressSearch = $.ajax('/wp-json/wp/v2/search/' + this.searchTerm);
+
+      $.when(ckanSearch, wordPressSearch).done(function(datasets, pages) {
+        // ckan search results
         self.datasetResults = []
-        data.result.results.map(function(result) {
+        datasets[0].result.results.map(function(result) {
           self.datasetResults.push({
             title: result.title[self.language],
             description: result.description[self.language],
             link: '/' + self.language + '/dataset/' + result.name
           })
         })
-      })
 
-      // wordpress search
-      $.ajax('/wp-json/wp/v2/search/' + this.searchTerm).done(function(data) {
+        // wordpress search results
         self.pageResults = []
-        data.map(function(result) {
+        pages[0].map(function(result) {
           self.pageResults.push({
             title: result.title.rendered,
             description: result.excerpt.rendered,
             link: result.link
           })
         })
-      })
 
+      })
     }
   }
 })
