16adec2cf2e12c26cf6013f7385df2a4ee37f017
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-08-18 17:19:13 +0200

    implemented wordpress provided navigation

--- ckanext/switzerland/helpers.py
+++ ckanext/switzerland/helpers.py
@@ -9,8 +9,8 @@ import json
 import pylons
 from ckan.lib.munge import munge_filename
 from pylons import config
-from ckan.common import _
-from ckan.lib.helpers import link_to, url_for
+from ckan.common import _, request
+from ckan.lib.helpers import link_to, url_for, lang
 from ckan.lib.helpers import dataset_display_name as dataset_display_name_orig
 import ast
 from ckan.common import c
@@ -356,14 +356,19 @@ def resource_filename(resource_url):
 
 
 def load_wordpress_templates():
-    # site_url = config.get('ckan.site_url', '')
-    # url = '{}/wordpress_api/templates?language={}'.format(site_url, lang())
-    # data = requests.get(url).json()
-    data = {
-        "header": "<div class=\"col-xs-6 left\"> <a href=\"/dashboard\" class=\"btn btn-link\" title=\"Dashboard\"> <span class=\"username\">Fabio Anderegg</span> </a> <a href=\"/logout\" title=\"Log out\" class=\"btn btn-link\"> <span class=\"text\">Log out</span> </a> </div> <div class=\"col-xs-6 right text-right\"> <a href=\"#\" class=\"btn btn-link\">DE</a> <a href=\"#\" class=\"btn btn-link\">FR</a> <a href=\"#\" class=\"btn btn-link\">IT</a> <a href=\"#\" class=\"btn btn-link\">EN</a> </div>",
-        "navigation": "<ul class=\"nav navbar-nav\"> <li><a href=\"/dataset\">Datasets</a></li> <li><a href=\"/organization\">Organizations</a></li> <li class=\"dropdown\"> <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">About the Portal <span class=\"caret\"></span></a> <ul class=\"dropdown-menu\"> <li><a href=\"/about\">About the Portal</a></li> <li><a href=\"/contact\">Contact</a></li> <li><a href=\"/terms-of-use\">Terms of Use</a></li> <li><a href=\"/netiquette\">Netiquette</a></li> </ul> </li> </ul>",
-        "footer": "<div class=\"col-xs-6 footer text-right\"> <a href=\"/netiquette\" class=\"btn btn-link text\">Netiquette</a> <a href=\"/terms-of-use\" class=\"btn btn-link text\">Terms of Use</a> <a href=\"/contact\" class=\"btn btn-link text\">Contact</a> <a href=\"/about\" class=\"btn btn-link text\">About</a> </div>"
-    }
-    c.wordpress_header = data['header']
-    c.wordpress_navigation = data['navigation']
+    site_url = config.get('ckan.site_url', '')
+    url = '{}/cms/wp-admin/admin-post.php?action=get_nav&language={}'.format(site_url, lang())
+    resp = requests.get(url, cookies=request.cookies)
+    if resp.status_code != 200:
+        return
+
+    try:
+        data = resp.json()['data']
+    except (ValueError, KeyError):
+        return
+
+    print data
+
+    c.wordpress_user_navigation = data['user']
+    c.wordpress_main_navigation = data['main']
     c.wordpress_footer = data['footer']
