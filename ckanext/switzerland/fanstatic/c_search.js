f1959a490f7e5666c4c71ac531a162f0f458d1ec
Lars Peyer <lars.peyer@liip.ch>
2016-10-28 13:59:29 +0200

    [Ckan] paginated search and added breadcrumb to search

--- ckanext/switzerland/fanstatic/c_search.js
+++ ckanext/switzerland/fanstatic/c_search.js
@@ -1,5 +1,6 @@
 Vue.config.delimiters = ['[[', ']]'];
 Vue.config.unsafeDelimiters = ['[[[', ']]]'];
+
 new Vue({
   el: '#app',
   data: {
@@ -7,7 +8,25 @@ new Vue({
     searchTerm: '',
     pageResults: [],
     datasetResults: [],
-    language: ''
+    language: '',
+    paginatePages: {
+      currentPage: 1,
+      itemsPerPage: 8,
+      resultCount: 0
+    },
+    paginateDatasets: {
+      currentPage: 1,
+      itemsPerPage: 5,
+      resultCount: 0
+    }
+  },
+  computed: {
+      totalPages: function() {
+        return Math.ceil(this.paginatePages.resultCount / this.paginatePages.itemsPerPage)
+      },
+      totalDatasets: function() {
+        return Math.ceil(this.paginateDatasets.resultCount / this.paginateDatasets.itemsPerPage)
+      }
   },
   ready: function() {
     var self = this
@@ -57,6 +76,23 @@ new Vue({
 
         self.currentSearchTerm = self.searchTerm
       })
+    },
+    setPage: function(pageNumber, paginate) {
+      paginate.currentPage = pageNumber
+    }
+  },
+  filters: {
+    paginate: function(list, paginate) {
+      paginate.resultCount = list.length;
+      if (paginate == this.paginatePages && paginate.currentPage >= this.totalPages) {
+        paginate.currentPage = this.totalPages
+      }
+      else if (paginate == this.paginateDatasets && paginate.currentPage >= this.totalDatasets) {
+        paginate.currentPage = this.totalDatasets
+      }
+      var index = paginate.currentPage * paginate.itemsPerPage
+      console.log(index);
+      return list.slice(index, index + paginate.itemsPerPage)
     }
   }
 })
