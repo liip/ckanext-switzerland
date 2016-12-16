466328108999f6a16efe6943b9ce5be4284c9f82
Fabio Anderegg <fabio.anderegg@liip.ch>
2016-12-16 16:30:19 +0100

    search: correctly decode url parameter with spaces in it

--- ckanext/switzerland/fanstatic/c_search.js
+++ ckanext/switzerland/fanstatic/c_search.js
@@ -51,7 +51,7 @@ new Vue({
       $.each(queryString.substring(1).split('&'), function(index, param) {
         var pair = param.split('=')
         if (pair[0] == 'q') {
-          self.searchTerm = decodeURIComponent(pair[1])
+          self.searchTerm = decodeURIComponent(pair[1].replace(/\+/g, '%20'))
           if (self.searchTerm.length >= 3) {
             self.search()
           }
