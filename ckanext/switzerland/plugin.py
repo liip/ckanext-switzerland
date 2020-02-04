d411f0d39e5bf0c53bc9225ca06c7df741ac8e96
Stefanie Taepke <stefanie.taepke@liip.ch>
2020-02-04 17:03:56 +0100

    fix imports of switzerland-helpers to override lib.helpers properly
    fixes translations on dashboard/organization-page

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -4,20 +4,8 @@ import sys
 
 import re
 from ckanext.switzerland import validators as v
-from ckanext.switzerland.logic import (
-    ogdch_dataset_count, ogdch_dataset_terms_of_use,
-    ogdch_dataset_by_identifier, ogdch_content_headers
-)
-from ckanext.switzerland.helpers import (
-    get_dataset_count, get_group_count, get_app_count,
-    get_org_count, get_tweet_count, get_localized_value,
-    get_localized_org, localize_json_title, get_langs,
-    get_frequency_name, get_terms_of_use_icon, get_dataset_terms_of_use,
-    get_dataset_by_identifier, get_readable_file_size,
-    simplify_terms_of_use, parse_json, get_piwik_config,
-    convert_post_data_to_dict, dataset_display_name, resource_display_name, group_link, resource_link,
-    parse_and_localize, resource_filename, load_wordpress_templates, render_description
-)
+from ckanext.switzerland import logic as l
+import ckanext.switzerland.helpers as sh
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -63,7 +51,7 @@ class OgdchPlugin(plugins.SingletonPlugin):
             'temporals_to_datetime_output': v.temporals_to_datetime_output,
             'json_list_of_dicts_field': v.json_list_of_dicts_field,
             'swiss_date': v.swiss_date,
-            'parse_json': parse_json,
+            'parse_json': sh.parse_json,
             'url': v.url_validator,
         }
 
@@ -102,10 +90,10 @@ class OgdchPlugin(plugins.SingletonPlugin):
         Expose new API methods
         """
         return {
-            'ogdch_dataset_count': ogdch_dataset_count,
-            'ogdch_dataset_terms_of_use': ogdch_dataset_terms_of_use,
-            'ogdch_dataset_by_identifier': ogdch_dataset_by_identifier,
-            'ogdch_content_headers': ogdch_content_headers,
+            'ogdch_dataset_count': l.ogdch_dataset_count,
+            'ogdch_dataset_terms_of_use': l.ogdch_dataset_terms_of_use,
+            'ogdch_dataset_by_identifier': l.ogdch_dataset_by_identifier,
+            'ogdch_content_headers': l.ogdch_content_headers,
         }
 
     # ITemplateHelpers
@@ -115,26 +103,31 @@ class OgdchPlugin(plugins.SingletonPlugin):
         Provide template helper functions
         """
         return {
-            'get_dataset_count': get_dataset_count,
-            'get_group_count': get_group_count,
-            'get_app_count': get_app_count,
-            'get_org_count': get_org_count,
-            'get_tweet_count': get_tweet_count,
-            'get_localized_org': get_localized_org,
-            'get_localized_value': get_localized_value,
-            'localize_json_title': localize_json_title,
-            'parse_and_localize': parse_and_localize,
-            'get_frequency_name': get_frequency_name,
-            'get_terms_of_use_icon': get_terms_of_use_icon,
-            'get_dataset_terms_of_use': get_dataset_terms_of_use,
-            'get_dataset_by_identifier': get_dataset_by_identifier,
-            'get_readable_file_size': get_readable_file_size,
-            'get_piwik_config': get_piwik_config,
-            'parse_json': parse_json,
-            'convert_post_data_to_dict': convert_post_data_to_dict,
-            'resource_filename': resource_filename,
-            'load_wordpress_templates': load_wordpress_templates,
-            'render_description': render_description,
+            'get_dataset_count': sh.get_dataset_count,
+            'get_group_count': sh.get_group_count,
+            'get_app_count': sh.get_app_count,
+            'get_org_count': sh.get_org_count,
+            'get_tweet_count': sh.get_tweet_count,
+            'get_localized_org': sh.get_localized_org,
+            'get_localized_value': sh.get_localized_value,
+            'localize_json_title': sh.localize_json_title,
+            'parse_and_localize': sh.parse_and_localize,
+            'get_frequency_name': sh.get_frequency_name,
+            'get_terms_of_use_icon': sh.get_terms_of_use_icon,
+            'get_dataset_terms_of_use': sh.get_dataset_terms_of_use,
+            'get_dataset_by_identifier': sh.get_dataset_by_identifier,
+            'get_readable_file_size': sh.get_readable_file_size,
+            'get_piwik_config': sh.get_piwik_config,
+            'parse_json': sh.parse_json,
+            'convert_post_data_to_dict': sh.convert_post_data_to_dict,
+            'resource_filename': sh.resource_filename,
+            'load_wordpress_templates': sh.load_wordpress_templates,
+            'render_description': sh.render_description,
+            # monkey patch template helpers to return translated names/titles
+            'dataset_display_name': sh.dataset_display_name,
+            'resource_display_name': sh.resource_display_name,
+            'group_link': sh.group_link,
+            'resource_link': sh.resource_link,
         }
 
     def i18n_directory(self):
@@ -202,7 +195,7 @@ class OgdchLanguagePlugin(plugins.SingletonPlugin):
     def _package_parse_json_strings(self, pkg_dict):
         # try to parse all values as JSON
         for key, value in pkg_dict.iteritems():
-            pkg_dict[key] = parse_json(value)
+            pkg_dict[key] = sh.parse_json(value)
 
         # groups
         if 'groups' in pkg_dict and pkg_dict['groups'] is not None:
@@ -213,12 +206,12 @@ class OgdchLanguagePlugin(plugins.SingletonPlugin):
                 """
                 group['title'] = group['display_name']
                 for field in group:
-                    group[field] = parse_json(group[field])
+                    group[field] = sh.parse_json(group[field])
 
         # organization
         if 'organization' in pkg_dict and pkg_dict['organization'] is not None:
             for field in pkg_dict['organization']:
-                pkg_dict['organization'][field] = parse_json(
+                pkg_dict['organization'][field] = sh.parse_json(
                     pkg_dict['organization'][field]
                 )
 
@@ -244,10 +237,10 @@ class OgdchLanguagePlugin(plugins.SingletonPlugin):
         return pkg_dict
 
     def _extract_lang_value(self, value, lang_code):
-        new_value = parse_json(value)
+        new_value = sh.parse_json(value)
 
         if isinstance(new_value, dict):
-            return get_localized_value(new_value, lang_code, default_value='')
+            return sh.get_localized_value(new_value, lang_code, default_value='')
         return value
 
     def _package_reduce_to_requested_language(self, pkg_dict, desired_lang_code):  # noqa
@@ -397,12 +390,12 @@ class OgdchPackagePlugin(OgdchLanguagePlugin):
                 """
                 group['title'] = group['display_name']
                 for field in group:
-                    group[field] = parse_json(group[field])
+                    group[field] = sh.parse_json(group[field])
 
         # organization
         if pkg_dict['organization'] is not None:
             for field in pkg_dict['organization']:
-                pkg_dict['organization'][field] = parse_json(
+                pkg_dict['organization'][field] = sh.parse_json(
                     pkg_dict['organization'][field]
                 )
 
@@ -419,29 +412,29 @@ class OgdchPackagePlugin(OgdchLanguagePlugin):
 
         search_data['res_name'] = [extract_title(r) for r in validated_dict[u'resources']]  # noqa
         search_data['res_format'] = [r['media_type'] for r in validated_dict[u'resources'] if 'media_type' in r]  # noqa
-        search_data['res_rights'] = [simplify_terms_of_use(r.get('rights', '')) for r in validated_dict[u'resources']]  # noqa
+        search_data['res_rights'] = [sh.simplify_terms_of_use(r.get('rights', '')) for r in validated_dict[u'resources']]  # noqa
         search_data['title_string'] = extract_title(validated_dict)
         search_data['description'] = LangToString('description')(validated_dict)  # noqa
 
         try:
             # index language-specific values (or it's fallback)
             text_field_items = {}
-            for lang_code in get_langs():
-                search_data['title_' + lang_code] = get_localized_value(
+            for lang_code in sh.get_langs():
+                search_data['title_' + lang_code] = sh.get_localized_value(
                     validated_dict['title'],
                     lang_code
                 )
                 search_data['title_string_' + lang_code] = munge_title_to_name(
-                    get_localized_value(validated_dict['title'], lang_code)
+                    sh.get_localized_value(validated_dict['title'], lang_code)
                 )
-                search_data['description_' + lang_code] = get_localized_value(
+                search_data['description_' + lang_code] = sh.get_localized_value(
                     validated_dict['description'],
                     lang_code
                 )
 
                 search_data['keywords_' + lang_code] = validated_dict['keywords'].get(lang_code)
 
-                text_field_items['text_' + lang_code] = [get_localized_value(validated_dict['description'], lang_code)]  # noqa
+                text_field_items['text_' + lang_code] = [sh.get_localized_value(validated_dict['description'], lang_code)]  # noqa
                 if search_data['keywords_' + lang_code]:
                     text_field_items['text_' + lang_code].append(' '.join(search_data['keywords_' + lang_code]))  # noqa
 
@@ -465,7 +458,7 @@ class OgdchPackagePlugin(OgdchLanguagePlugin):
         search in correct language-specific field and boost
         results in current language
         '''
-        lang_set = get_langs()
+        lang_set = sh.get_langs()
         try:
             current_lang = toolkit.request.environ['CKAN_LANG']
         except TypeError as err:
@@ -527,12 +520,6 @@ class LangToString(object):
             return ''
         except AttributeError:
             return None
-
-# monkey patch template helpers to return translated names/titles
-h.dataset_display_name = dataset_display_name
-h.resource_display_name = resource_display_name
-h.group_link = group_link
-h.resource_link = resource_link
 
 
 # Monkeypatch to style CKAN pagination
