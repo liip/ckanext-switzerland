8e67e471b7e9fb536cdca6ac1f3ee4e5f382b3d6
Benoit Potty <benoit.potty@liip.ch>
2022-12-21 09:08:28 +0100

    refactor: clean code

--- ckanext/switzerland/tests/test_s3_storage_adapter.py
+++ ckanext/switzerland/tests/test_s3_storage_adapter.py
@@ -2,15 +2,13 @@ import unittest
 import os
 import shutil
 
-from nose.tools import assert_equal
-
 # The classes to test
 # -----------------------------------------------------------------------
 from ckanext.switzerland.harvester.s3_storage_adapter import S3StorageAdapter
 # -----------------------------------------------------------------------
 
 class TestS3StorageAdapter(unittest.TestCase):
-    tmpfolder = '/tmp/s3harvest/tests/'
+    temp_folder = '/tmp/s3harvest/tests/'
 
     @classmethod
     def setup_class(cls):
@@ -21,39 +19,37 @@ class TestS3StorageAdapter(unittest.TestCase):
         pass
 
     def setup(self):
-        if os.path.exists(self.tmpfolder):
-            shutil.rmtree(self.tmpfolder, ignore_errors=True)
+        if os.path.exists(self.temp_folder):
+            shutil.rmtree(self.temp_folder, ignore_errors=True)
 
     def teardown(self):
         # remove the tmp directory
-        if os.path.exists(self.tmpfolder):
-            shutil.rmtree(self.tmpfolder, ignore_errors=True)
+        if os.path.exists(self.temp_folder):
+            shutil.rmtree(self.temp_folder, ignore_errors=True)
     
     def test_init_when_remote_folder_then_stored(self):
         remote_folder = '/test/'
 
         storage_adapter = S3StorageAdapter(remote_folder)
 
-        assert_equal('/test', storage_adapter.remote_folder)
+        self.assertEqual('/test', storage_adapter.remote_folder)
     
     def test_init_without_remote_folder_then_empty(self):
         config = {}
 
         storage_adapter = S3StorageAdapter(config=config)
 
-        assert_equal('', storage_adapter.remote_folder)
+        self.assertEqual('', storage_adapter.remote_folder)
 
     def test_init_when_config_then_stored(self):
         config = {}
 
         storage_adapter = S3StorageAdapter(config=config)
 
-        assert_equal(config, storage_adapter.config)
+        self.assertEqual(config, storage_adapter.config)
     
-    def test_init_without_config_then_none(self):
+    def test_init_without_config_then_exception_is_raised(self):
         remote_folder = '/test/'
 
-        storage_adapter = S3StorageAdapter(remote_folder)
-
-        assert storage_adapter.config is None
+        self.assertRaises(Exception, S3StorageAdapter(remote_folder))
 
