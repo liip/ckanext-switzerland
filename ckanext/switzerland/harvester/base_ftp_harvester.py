87b9e1a7b44ca9c030e5c211145059166ba92c62
Nataliia Kovalchuk <nataliia.kovalchuk@liip.ch>
2023-01-20 10:40:42 +0100

    fix: adapt harvester config possibility via config.get or validate_config

--- ckanext/switzerland/harvester/base_ftp_harvester.py
+++ ckanext/switzerland/harvester/base_ftp_harvester.py
@@ -488,7 +488,10 @@ class BaseFTPHarvester(HarvesterBase):
         log.info("Local directory: %s", tmpfolder)
 
         ftp_config = {}
-        ftp_config['ftp_server'] = self.config.get('ftp_server')
+        try:
+            ftp_config['ftp_server'] = self.validate_config('ftp_server')
+        except JSONDecodeError:
+            ftp_config['ftp_server'] = self.config.get('ftp_server')
 
         try:
 
