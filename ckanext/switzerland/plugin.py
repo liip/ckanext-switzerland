821285e3e864d4a6f00917884d0774cd820b3089
Rae Knowler <rae.knowler@liip.ch>
2023-10-18 15:51:27 +0200

    style: Fix unicode use for Python 3 (using 2to3)

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -374,9 +374,9 @@ class OgdchPackagePlugin(OgdchLanguagePlugin):
 
         # log.debug(pprint.pformat(validated_dict))
 
-        search_data['res_name'] = [extract_title(r) for r in validated_dict[u'resources']]  # noqa
-        search_data['res_format'] = [r['media_type'] for r in validated_dict[u'resources'] if 'media_type' in r]  # noqa
-        search_data['res_rights'] = [sh.simplify_terms_of_use(r.get('rights', '')) for r in validated_dict[u'resources']]  # noqa
+        search_data['res_name'] = [extract_title(r) for r in validated_dict['resources']]  # noqa
+        search_data['res_format'] = [r['media_type'] for r in validated_dict['resources'] if 'media_type' in r]  # noqa
+        search_data['res_rights'] = [sh.simplify_terms_of_use(r.get('rights', '')) for r in validated_dict['resources']]  # noqa
         search_data['title_string'] = extract_title(validated_dict)
         search_data['description'] = LangToString('description')(validated_dict)  # noqa
 
