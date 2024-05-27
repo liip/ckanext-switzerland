b230cd3de8c6d35a807562d361297aeb25be9984
Rae Knowler <rae.knowler@liip.ch>
2024-05-27 15:49:29 +0200

    tests: Use time-machine for mocking datetime

--- ckanext/switzerland/tests/test_plugin.py
+++ ckanext/switzerland/tests/test_plugin.py
@@ -1,9 +1,12 @@
 # encoding: utf-8
+import datetime
 import json
 import logging
-import pytest
+from zoneinfo import ZoneInfo
 
 import ckan.tests.helpers as helpers
+import pytest
+import time_machine
 from ckan.lib.helpers import url_for
 
 from . import data
@@ -13,21 +16,27 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.usefixtures("clean_db", "clean_index", "with_plugins")
 class TestOgdchPackagePlugin(object):
-
+    @time_machine.travel(
+        datetime.datetime(2022, 4, 20, 14, 15, 0, 0, ZoneInfo("UTC")), tick=False
+    )
     def test_get_correct_datetime_format_from_api(self, app, ckan_config):
+        """Create a dataset with a resource, set datetime fields to known values, and
+        check that when we get the dataset from the API, all datetimes have the correct
+        values and are output in the timezone Europe/Zurich.
+
+        We mock the current time as 14:15 UTC, because CKAN sets the values for
+        'metadata_created' and 'metadata_modified' to datetime.datetime.utcnow() when
+        creating or modifying a dataset/resource.
+        """
         dataset_datetime_fields = {
-            "metadata_created": "2022-04-20T14:15:00",
-            "metadata_modified": "2022-04-20T14:30:00",
-            "issued": "2022-04-20T16:15:00",
-            "modified": "2022-04-20T16:30:00",
-            "version": "2022-04-20T16:30:00",
+            "issued": "2022-04-20T12:00:00",
+            "modified": "2022-04-20T12:30:00",
+            "version": "2022-04-20T12:30:00",
         }
         resource_datetime_fields = {
-            "created": "2022-04-20T14:15:00",
-            "last_modified": "2022-04-20T14:30:00",
-            "metadata_modified": "2022-04-20T14:30:00",
-            "issued": "2022-04-20T16:15:00",
-            "modified": "2022-04-20T16:30:00",
+            "last_modified": "2022-04-20T14:15:00",
+            "issued": "2022-04-20T12:00:00",
+            "modified": "2022-04-20T12:30:00",
         }
         dataset = data.dataset()
         resource = data.resource(dataset=dataset)
@@ -35,8 +44,8 @@ class TestOgdchPackagePlugin(object):
         dataset.update(**dataset_datetime_fields)
         resource.update(**resource_datetime_fields)
 
-        helpers.call_action('package_update', **dataset)
-        helpers.call_action('resource_create', **resource)
+        helpers.call_action("package_update", **dataset)
+        helpers.call_action("resource_create", **resource)
 
         resp = app.get(
             url_for(
@@ -44,24 +53,20 @@ class TestOgdchPackagePlugin(object):
                 logic_function="package_show",
                 ver=3,
                 id=dataset["name"],
-                status=200
+                status=200,
             )
         )
         pkg_dict = json.loads(resp.body)["result"]
         resource_dict = pkg_dict["resources"][0]
 
-        # Fields set by CKAN
         assert pkg_dict["metadata_created"] == "2022-04-20T16:15:00+02:00"
-        assert pkg_dict["metadata_modified"] == "2022-04-20T16:30:00+02:00"
-        # Our custom fields
-        assert pkg_dict["issued"] == "2022-04-20T16:15:00+02:00"
-        assert pkg_dict["modified"] == "2022-04-20T16:30:00+02:00"
-        assert pkg_dict["version"] == "2022-04-20T16:30:00+02:00"
+        assert pkg_dict["metadata_modified"] == "2022-04-20T16:15:00+02:00"
+        assert pkg_dict["issued"] == "2022-04-20T12:00:00+02:00"
+        assert pkg_dict["modified"] == "2022-04-20T12:30:00+02:00"
+        assert pkg_dict["version"] == "2022-04-20T12:30:00+02:00"
 
-        # Fields set by CKAN
         assert resource_dict["created"] == "2022-04-20T16:15:00+02:00"
-        assert resource_dict["last_modified"] == "2022-04-20T16:30:00+02:00"
-        assert resource_dict["metadata_modified"] == "2022-04-20T16:30:00+02:00"
-        # Our custom fields
-        assert resource_dict["issued"] == "2022-04-20T16:15:00+02:00"
-        assert resource_dict["modified"] == "2022-04-20T16:30:00+02:00"
+        assert resource_dict["metadata_modified"] == "2022-04-20T16:15:00+02:00"
+        assert resource_dict["last_modified"] == "2022-04-20T16:15:00+02:00"
+        assert resource_dict["issued"] == "2022-04-20T12:00:00+02:00"
+        assert resource_dict["modified"] == "2022-04-20T12:30:00+02:00"
