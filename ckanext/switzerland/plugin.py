a1ed1a2f6e3b39206831a7f4de5d73b68eb848ff
Rae Knowler <rae.knowler@liip.ch>
2024-10-30 11:38:54 +0100

    chore: Clean up no-longer-used actions

--- ckanext/switzerland/plugin.py
+++ ckanext/switzerland/plugin.py
@@ -57,10 +57,7 @@ class OgdchPlugin(plugins.SingletonPlugin):
         Expose new API methods
         """
         return {
-            "ogdch_dataset_count": l.ogdch_dataset_count,
-            "ogdch_dataset_terms_of_use": l.ogdch_dataset_terms_of_use,
             "ogdch_dataset_by_identifier": l.ogdch_dataset_by_identifier,
-            "ogdch_content_headers": l.ogdch_content_headers,
         }
 
     # ITemplateHelpers
