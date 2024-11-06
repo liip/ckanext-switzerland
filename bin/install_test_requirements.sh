c953c6481c32f35e69058598d2686f45df0bb838
Rae Knowler <rae.knowler@liip.ch>
2024-11-06 18:30:29 +0100

    tests: Add debug statements

--- bin/install_test_requirements.sh
+++ bin/install_test_requirements.sh
@@ -23,5 +23,5 @@ ckan -c /__w/ckanext-switzerland/ckanext-switzerland/test.ini db init
 ckan config-tool /__w/ckanext-switzerland/ckanext-switzerland/test.ini "ckan.plugins = ogdch ogdch_pkg ogdch_group ogdch_org harvest timetable_harvester sbb_harvester datastore fluent scheming_datasets scheming_groups scheming_organizations"
 ckan -c /__w/ckanext-switzerland/ckanext-switzerland/test.ini db pending-migrations --apply
 
-ckan config search --with-current ckan.plugins
+ckan -c /__w/ckanext-switzerland/ckanext-switzerland/test.ini config search --with-current ckan.plugins
 cat /__w/ckanext-switzerland/ckanext-switzerland/test.ini
