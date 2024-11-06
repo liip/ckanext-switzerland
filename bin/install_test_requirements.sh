60ae239f4cd499c9c5684d91d802f350a532ba05
Rae Knowler <rae.knowler@liip.ch>
2024-11-06 18:55:29 +0100

    tests: Use most recent versions of ckanexts

--- bin/install_test_requirements.sh
+++ bin/install_test_requirements.sh
@@ -6,11 +6,11 @@ pip install --ignore-installed -r /__w/ckanext-switzerland/ckanext-switzerland/d
 pip install -e /__w/ckanext-switzerland/ckanext-switzerland/
 
 # Install ckanext dependencies
-pip install -e git+https://github.com/ckan/ckanext-dcat.git@v1.5.1#egg=ckanext-dcat
-pip install -r https://raw.githubusercontent.com/ckan/ckanext-dcat/v1.5.1/requirements.txt
+pip install -e git+https://github.com/ckan/ckanext-dcat.git#egg=ckanext-dcat
+pip install -r https://raw.githubusercontent.com/ckan/ckanext-dcat/master/requirements.txt
 pip install -e git+https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest.git#egg=ckanext-harvest
 pip install -r https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest/-/raw/main/requirements.txt
-pip install -e git+https://github.com/ckan/ckanext-scheming.git@release-3.0.0#egg=ckanext-scheming
+pip install -e git+https://github.com/ckan/ckanext-scheming.git#egg=ckanext-scheming
 pip install -e git+https://github.com/ckan/ckanext-fluent.git#egg=ckanext-fluent
 pip install -r https://raw.githubusercontent.com/ckan/ckanext-fluent/master/requirements.txt
 
