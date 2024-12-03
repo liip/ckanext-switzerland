0b6203ac5650e3541b318969e5c46103ce6aa5cb
Rae Knowler <rae.knowler@liip.ch>
2024-12-03 17:27:57 +0100

    tests: Pin ckanext-harvest to non-breaking version
    v1.6.0 included changes that required updates on our side, which were made in the migration branch of ckanext-switzerland. No point adding the same changes here as the migration will soon be finished and the migration branch will be merged into main.

--- bin/install_test_requirements.sh
+++ bin/install_test_requirements.sh
@@ -8,8 +8,8 @@ pip install -e /__w/ckanext-switzerland/ckanext-switzerland/
 # Install ckanext dependencies
 pip install -e git+https://github.com/ckan/ckanext-dcat.git@v1.5.1#egg=ckanext-dcat
 pip install -r https://raw.githubusercontent.com/ckan/ckanext-dcat/v1.5.1/requirements.txt
-pip install -e git+https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest.git#egg=ckanext-harvest
-pip install -r https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest/-/raw/main/requirements.txt
+pip install -e git+https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest.git@v1.5.6#egg=ckanext-harvest
+pip install -r https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest/v1.5.6/requirements.txt
 pip install -e git+https://github.com/ckan/ckanext-scheming.git@release-3.0.0#egg=ckanext-scheming
 pip install -e git+https://github.com/ckan/ckanext-fluent.git#egg=ckanext-fluent
 pip install -r https://raw.githubusercontent.com/ckan/ckanext-fluent/master/requirements.txt
