b875caab9efc834321bd5c49afb4109f6bc64d75
Rae Knowler <rae.knowler@liip.ch>
2025-12-04 15:29:32 +0100

    feat: Update setup for github tests
    - Install package the pyproject.toml way
    - Don't separately install requirements
    - Don't separately install requirements for ckanext-dcat as they are also using pyproject.toml now
    - Don't apply db migrations separately, as they are done in db init

--- bin/install_test_requirements.sh
+++ bin/install_test_requirements.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Install requirements and ckanext
-pip install -r /__w/ckanext-switzerland/ckanext-switzerland/requirements.txt
-pip install --ignore-installed -r /__w/ckanext-switzerland/ckanext-switzerland/dev-requirements.txt
-pip install -e /__w/ckanext-switzerland/ckanext-switzerland/
+pip install -e /__w/ckanext-switzerland/ckanext-switzerland/[dev]
 
 # Install ckanext dependencies
 pip install -e git+https://github.com/ckan/ckanext-dcat.git#egg=ckanext-dcat
-pip install -r https://raw.githubusercontent.com/ckan/ckanext-dcat/master/requirements.txt
 pip install -e git+https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest.git#egg=ckanext-harvest
 pip install -r https://gitlab.liip.ch/odp_oev_schweiz/ckanext-harvest/-/raw/main/requirements.txt
 pip install -e git+https://github.com/ckan/ckanext-scheming.git#egg=ckanext-scheming
@@ -20,4 +17,3 @@ sed -i -e 's/use = config:.*/use = config:\/srv\/app\/src\/ckan\/test-core.ini/'
 
 # Init db and re-enable required plugins
 ckan -c /__w/ckanext-switzerland/ckanext-switzerland/test.ini db init
-ckan -c /__w/ckanext-switzerland/ckanext-switzerland/test.ini db pending-migrations --apply
