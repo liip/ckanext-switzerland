ef093e0c3e538d884ce1e579c85782171f6bb5ca
Stefanie Taepke <stefina@users.noreply.github.com>
2026-08-05 16:25:54 +0200

    feat: add robots-meta-content

--- ckanext/switzerland/tests/test_helpers.py
+++ ckanext/switzerland/tests/test_helpers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ckanext.switzerland.helpers import map_to_valid_format
+from ckanext.switzerland.helpers import get_robots_meta_content, map_to_valid_format
 
 CSV_URI = "http://publications.europa.eu/resource/authority/file-type/CSV"
 GEOJSON_URI = "http://publications.europa.eu/resource/authority/file-type/GEOJSON"
@@ -61,3 +61,95 @@ INVALID_FORMAT = None
 )
 def test_map_to_valid_format_known_values(input_format, expected):
     assert map_to_valid_format(input_format) == expected
+
+
+INDEX = "index, follow"
+NOINDEX = "noindex, follow"
+
+
+class _FakeArgs(dict):
+    """Minimal stand-in for Flask request.args (truthy when non-empty)."""
+
+    def __bool__(self):
+        return bool(dict(self))
+
+
+class _FakeRequest:
+    def __init__(self, endpoint, path="/", args=None):
+        self.endpoint = endpoint
+        self.path = path
+        self.args = _FakeArgs(args or {})
+
+
+@pytest.mark.parametrize(
+    "endpoint,path,args,expected",
+    [
+        ("dataset.search", "/dataset/", None, INDEX),
+        ("dataset.search", "/dataset/", {"q": "fahrplan"}, NOINDEX),
+        ("dataset.search", "/dataset/", {"groups": "timetables"}, NOINDEX),
+        ("dataset.search", "/dataset/", {"page": "2"}, NOINDEX),
+        ("ogdch_home.search", "/", None, INDEX),
+        ("ogdch_home.search", "/", {"organization": "oevch"}, NOINDEX),
+        ("dataset.read", "/dataset/bike-and-car-parking", None, INDEX),
+        ("showcase_blueprint.index", "/showcase/", None, INDEX),
+        (
+            "showcase_blueprint.index",
+            "/showcase/",
+            {"q": "viz"},
+            NOINDEX,
+        ),
+        (
+            "showcase_blueprint.read",
+            "/showcase/visualisierung-der-oev-tagesentwicklungen",
+            None,
+            INDEX,
+        ),
+        ("group.index", "/group/", None, INDEX),
+        ("group.read", "/group/accessibilitydata", None, INDEX),
+        (
+            "group.read",
+            "/group/accessibilitydata",
+            {"q": "test"},
+            NOINDEX,
+        ),
+        ("organization.index", "/organization/", None, NOINDEX),
+        ("organization.read", "/organization/oevch", None, NOINDEX),
+        (
+            "organization.read",
+            "/organization/oevch",
+            {"q": "x"},
+            NOINDEX,
+        ),
+        ("harvest.search", "/harvest/", None, NOINDEX),
+        ("harvest.read", "/harvest/some-source", None, NOINDEX),
+        # Path fallback when endpoint is missing/unknown
+        (None, "/harvest/", None, NOINDEX),
+        (None, "/organization/oevch", None, NOINDEX),
+        (
+            "dataset_resource.read",
+            "/dataset/bike-and-car-parking/resource/eb892409-d24f-4484-a19b-2d6e26108d9a",
+            None,
+            NOINDEX,
+        ),
+        (
+            "resource.read",
+            "/dataset/bike-and-car-parking/resource/eb892409-d24f-4484-a19b-2d6e26108d9a",
+            None,
+            NOINDEX,
+        ),
+        # Path fallback when endpoint is missing/unknown but URL is a resource
+        (
+            None,
+            "/dataset/bike-and-car-parking/resource/eb892409-d24f-4484-a19b-2d6e26108d9a",
+            None,
+            NOINDEX,
+        ),
+        ("user.read", "/user/admin", None, INDEX),
+    ],
+)
+def test_get_robots_meta_content(monkeypatch, endpoint, path, args, expected):
+    monkeypatch.setattr(
+        "ckanext.switzerland.helpers.tk.request",
+        _FakeRequest(endpoint, path, args),
+    )
+    assert get_robots_meta_content() == expected
